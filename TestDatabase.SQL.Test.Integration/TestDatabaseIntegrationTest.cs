00ac1eae15950a6dd6a1c65abcef61d7dc210ed9
Andreas Alminde <alminde7@hotmail.com>
2017-06-30 20:04:31 +0200

    Started integration testing

---
+++ TestDatabase.SQL.Test.Integration/TestDatabaseIntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestDatabase.SQL.Test.Integration
+{
+    [TestFixture]
+    public class TestDatabaseIntegrationTest
+    {
+        [Test]
+        public void Constructor_CreateNewInstanceOfObject_DbNameAndConnectionStringHasBeenSet()
+        {
+            var dbName = "Test";
+
+            var testDb = new TestDatabase(dbName, Config.ConnectionString);
+
+            Assert.NotNull(testDb.DbName);
+            Assert.That(testDb.Connectionstring, Is.EqualTo(Config.ConnectionString));
+        }
+
+    }
+}
